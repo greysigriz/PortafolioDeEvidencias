7772b6a227897787c855079ca39c1ee80c7902a0
greysi <60203884+greysigriz@users.noreply.github.com>
2024-02-03 18:03:36 -0500

    Add files via upload

---
+++ bitacora/scripts/4.py
@@ -0,0 +1,13 @@
+import sys
+import socket
+import getopt
+import threading
+import subprocess
+# define some global variables
+listen = False
+command = False
+upload = False
+execute = ""
+target = ""
+upload_destination = ""
+port = 0
